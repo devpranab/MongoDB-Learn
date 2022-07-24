6d7ef73d94e36fac48b1326bca106a3564fd2d1f
pranav s <dev.pranabsk@gmail.com>
2022-07-24 15:06:35 +0530

    Mongoose Schema

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ const mongoose = require("mongoose");
 mongoose.connect('mongodb://localhost:27017/my-students')
 .then(() => console.log("Connect to MongoDB Successfully!"))
 .catch(err => console.err("Connection Failed!"));
+
+
+//Mongoose Schema(define the shapes documents)
+const studentSchema = new mongoose.Schema({
+   firstName: {type: String},
+   lastName: String,
+   dob: Date,
+   entryDate: {type: Date, default: Date.now},
+   passed: Boolean,
+   hobbies: [String],
+   parents: {
+    father: String,
+    mother: String
+   },
+   subjects: [{name: String, marks: {type: Number, min: 0, max: 100}}]
+});
