7b2ec089d301feda7d17abbdb387c2bb73566835
pranav s <dev.pranabsk@gmail.com>
2022-07-24 20:03:40 +0530

    Querying Documents

--- app.js
+++ app.js
@@ -29,10 +29,14 @@ const studentSchema = new mongoose.Schema({
 //Model(class of schema)
 //Creating Mongoose Model And Saving Document
 
-const Student = mongoose.model("StudentDB", studentSchema); //class
+const Student = mongoose.model("Student", studentSchema); //class
+
+//C=create
+//handle using async await
+async function createStudent(){
 const student = new Student({
-    firstName: "Pranav",
-    lastName: "Sarkar",
+    firstName: "Ashis",
+    lastName: "Mandal",
     dob: new Date("25 June 2022"),
     passed: true,
     hobbies: ["Singing", "Coding"],
@@ -40,9 +44,26 @@ const student = new Student({
         father: "A",
         mother: "B"
        },
-    subjects: [{name: "Math", marks: 85}, {name: "Eng", marks: 80}],
+    subjects: [{name: "Math", marks: 85}, {name: "Eng", marks: 80}]
 })
+ try{
+    const data = await student.save();
+    console.log(data);
+ } catch(err){
+    console.log(err._message);
+ }
+}
 
-student.save() //save() built-in method
-.then(data => console.log(data))
-.catch(err => console.log(err));
+//createStudent();
+
+//R=read
+async function readStudents(){
+    const studentsData = await Student.find();
+    //const studentsData = await Student.find({firstName: "Pranay"});
+    //const studentsData = await Student.find().limit(2);
+    //const studentsData = await Student.find().sort({firstName: -1});
+    //const studentsData = await Student.find().select({firstName: 1, lastName: 1, hobbies: 1});
+    console.log(studentsData);
+}
+
+readStudents();
